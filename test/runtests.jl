ecfd8977115118d1149841984e9fb77b4128eb53
Zachary Sunberg <sunbergzach@gmail.com>
2017-11-20 10:08:14 -0800

    discrete seems to work

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,16 +14,26 @@ pomdp = SubHuntPOMDP()
 rng = MersenneTwister(6)
 policy = RandomPolicy(pomdp, rng=rng)
 
-solver = QMDPSolver()
-policy = solve(solver, pomdp, verbose=true)
-
-s = initial_state(pomdp, rng)
-@show value(policy, s)
-@show value(policy, SubState(s.own, s.target, s.goal, true))
+# solver = QMDPSolver()
+# policy = solve(solver, pomdp, verbose=true)
+# 
+# s = initial_state(pomdp, rng)
+# @show value(policy, s)
+# @show value(policy, SubState(s.own, s.target, s.goal, true))
 
 filter = SIRParticleFilter(pomdp, 10000, rng=rng)
 
 for (s, a, r, sp) in stepthrough(pomdp, policy, filter, "s,a,r,sp", max_steps=200, rng=rng)
     v = SubVis(pomdp, s=s, a=a, r=r)
+    show(STDOUT, MIME("text/plain"), v)
+end
+
+dpomdp = DSubHuntPOMDP(SubHuntPOMDP(), 1.0)
+
+policy = RandomPolicy(dpomdp, rng=rng)
+filter = SIRParticleFilter(dpomdp, 10000, rng=rng)
+
+for (s, a, r, sp) in stepthrough(dpomdp, policy, filter, "s,a,r,sp", max_steps=200, rng=rng)
+    v = SubVis(dpomdp.cp, s=s, a=a, r=r)
     show(STDOUT, MIME("text/plain"), v)
 end
