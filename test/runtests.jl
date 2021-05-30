71d61196af700a5491fdb3493224ab2f92bfbabe
WhiffleFish <tylertronics@gmail.com>
2021-05-29 21:25:50 -0400

    replace deprecated functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,7 +17,7 @@ using QMDP
     solver = ValueIterationSolver(verbose=true)
     vipolicy = solve(solver, UnderlyingMDP(pomdp))
     # 
-    s = initialstate(pomdp, rng)
+    s = rand(rng, initialstate(pomdp))
     @show value(vipolicy, s)
     @show value(vipolicy, SubState(s.own, s.target, s.goal, true))
 end
@@ -29,7 +29,7 @@ end
 
     # policy = RandomPolicy(pomdp, rng=rng)
     
-    filter = SIRParticleFilter(pomdp, 10000, rng=rng)
+    filter = BootstrapFilter(pomdp, 10000, rng)
 
     for (s, a, r, sp) in stepthrough(pomdp, policy, filter, "s,a,r,sp", max_steps=200, rng=rng)
         v = SubVis(pomdp, s=s, a=a, r=r)
@@ -41,7 +41,7 @@ end
     rng = MersenneTwister(6)
     dpomdp = DSubHuntPOMDP(SubHuntPOMDP(), 1.0)
     policy = RandomPolicy(dpomdp, rng=rng)
-    filter = SIRParticleFilter(dpomdp, 10000, rng=rng)
+    filter = BootstrapFilter(dpomdp, 10000, rng)
 
     for (s, a, r, sp) in stepthrough(dpomdp, policy, filter, "s,a,r,sp", max_steps=200, rng=rng)
         v = SubVis(dpomdp.cp, s=s, a=a, r=r)
@@ -56,7 +56,7 @@ end
 
     # policy = RandomPolicy(pomdp, rng=rng)
     
-    filter = SIRParticleFilter(pomdp, 10000, rng=rng)
+    filter = BootstrapFilter(pomdp, 10000, rng)
 
     for step in stepthrough(pomdp, policy, filter, "s,a,r,sp", max_steps=200, rng=rng)
         show(stdout, MIME("text/plain"), render(pomdp, step))
